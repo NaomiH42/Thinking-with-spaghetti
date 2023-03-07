928d453de9dd0f642275052f5e7aeccc94e7e803
NaomiH42 <119932323+NaomiH42@users.noreply.github.com>
2023-03-07 21:22:33 +0100

    PHILOSOPHERS 3 IS SLEEPING

--- main.c
+++ main.c
@@ -2,35 +2,100 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-void	*routine(void *time)
+typedef struct	s_info
 {
+	int	ttd;
+	int	tte;
+	int	tts;
+	int	phil_n;
+	int	*forks;
+}	t_info;
+
+typedef struct	s_id
+{
+	t_info *info;
+	int	id;
+}	t_id;
+
+void	*routine(void *phil_id)
+{
+	t_id	*id = (t_id*)phil_id;
 	struct timeval	timenow;
-	struct timeval	time;
+	struct timeval	last_eat;
 	int	ded;
 
 	ded = 0;
 	while (!ded)
 	{
-		gettimeofday(&timenow, NULL);
-		if ((timenow.tv_usec - (*struct)time.tv_usec) > 500)
+		if (id->id == (id->info->phil_n - 1))
+		{
+			if (id->info->forks[id->id] && id->info->forks[0])
+			{
+				id->info->forks[id->id] = 0;
+				id->info->forks[0] = 0;
+				printf("Philosopher %d is eating\n", id->id);
+				usleep(id->info->tte);
+				gettimeofday(&last_eat, NULL);
+				id->info->forks[id->id] = 1;
+				id->info->forks[0] = 1;
+				printf("Philosopher %d is sleeping\n", id->id);
+				usleep(id->info->tts);
+			}
+		}
+		else
+		{
+			if (id->info->forks[id->id] && id->info->forks[id->id - 1])
+			{
+				id->info->forks[id->id] = 0;
+				id->info->forks[0] = 0;
+				usleep(id->info->tte);
+				printf("Philosopher %d is eating\n", id->id);
+				gettimeofday(&last_eat, NULL);
+				id->info->forks[id->id] = 1;
+				id->info->forks[0] = 1;
+				printf("Philosopher %d is sleeping\n", id->id);
+				usleep(id->info->tts);
+			}
+		}
+		if (timenow.tv_usec - last_eat.tv_usec > id->info->ttd)
 			ded = 1;
 	}
-	gettimeofday(&timenow, NULL);
+	printf("Dude ded");
 }
 
 int	main(void)
 {
-	pthread_t		phil;
-	int				num_of_phi;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	struct timeval	time;
+	t_info			phil_info;
+	int				i;
+	t_id			*phil_id;
 
-	long			timestart;
-
-	gettimeofday(&time, NULL);
-	pthread_create(&phil, NULL, &routine, &time);
-	pthread_join(phil, NULL);
+	i = 0;
+	phil_info.phil_n = 5;
+	pthread_t		*phil = malloc(phil_info.phil_n * sizeof(pthread_t));
+	phil_info.ttd = 100000000;
+	phil_info.tts = 5000000;
+	phil_info.tte = 5000000;
+	phil_info.forks = malloc(phil_info.phil_n);
+	phil_id = malloc(phil_info.phil_n * sizeof(t_id));
+	while (i < phil_info.phil_n)
+	{
+		phil_info.forks[i] = 1;
+		phil_id[i].info = &phil_info;
+		phil_id[i].id = i;
+		i++;
+	}
+	i = 0;
+	while (i < phil_info.phil_n)
+	{
+		pthread_create(phil + i, NULL, &routine, &phil_id[i]);
+		i++;
+	}
+	i = 0;
+	while (i < phil_info.phil_n)
+	{
+		pthread_join(phil[i], NULL);
+		i++;
+	}
 }
